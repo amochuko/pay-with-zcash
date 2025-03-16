779b4b41f7def43a859408b5d6d0d5c260efd062
ochuko awhe <7204868+amochuko@users.noreply.github.com>
2025-03-16 11:48:12 +0100

    feat: add server actions

---
+++ src/app/lib/actions.ts
@@ -0,0 +1,13 @@
+'use server'
+
+export async function postMerchantListing(formData:FormData) {
+    console.log(formData)
+}
+
+export async function approveMerchant(formData:FormData) {
+    console.log(formData)
+}
+
+export async function deleteMerchant(formData:FormData) {
+    console.log(formData)
+}
