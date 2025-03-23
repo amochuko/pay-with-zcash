ade93c6ae6b61007c274aa7129114fc503a8ba36
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-23 14:51:06 +0100

    update: add _addDataToCategoryTable

--- src/scripts/database/databaseSeeding.ts
+++ src/scripts/database/databaseSeeding.ts
@@ -1,7 +1,7 @@
-// import categoriesJson from "./data/category.json";
 import sql from "../../app/lib/database/sqlConnection";
 import CategoryService from "../../app/lib/service/category.service";
 import MerchantService from "../../app/lib/service/merchant.service";
+import categoriesJson from "./data/category.json";
 
 export default class DatabaseSeeding {
   merchantService: MerchantService;
@@ -26,9 +26,8 @@ export default class DatabaseSeeding {
 
   async _createCategoryTable() {
     try {
-      await sql`DROP TABLE IF EXISTS categories;`;
-
       await sql`
+        DROP TABLE IF EXISTS categories;
         CREATE TABLE IF NOT EXISTS categories(
             category_id uuid NOT NULL DEFAULT gen_random_uuid(),
             name VARCHAR(120) NOT NULL,
@@ -37,9 +36,9 @@ export default class DatabaseSeeding {
 
             CONSTRAINT category_pkey PRIMARY KEY (category_id)
         );
-      `;
+      `.simple() /** Executing two statement at once with `.simple()` */;
 
-      console.log("Categories table created successfully")
+      console.log("Categories table created successfully");
       return true;
     } catch (err) {
       if (err instanceof Error) {
@@ -50,9 +49,35 @@ export default class DatabaseSeeding {
     }
   }
 
+  async _addDataToCategoryTable() {
+    const catObjArr: Record<string, string>[] = [];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Object.entries(categoriesJson).map(([_, value]) => {
+      catObjArr.push({ name: value });
+    });
+
+    console.log("Seeding Category table with: \n", catObjArr);
+
+    try {
+      await sql`insert into categories ${sql(catObjArr, "name")}`;
+      console.log("Seeding Category table was successful!");
+    } catch (err) {
+      if (err instanceof Error) {
+        throw err;
+      }
+
+      throw new Error("Seeding Category failed with");
+    }
+  }
+
   async seedCategoryTable() {
     try {
-      await this._createCategoryTable();
+      const result = await this._createCategoryTable();
+
+      if (result) {
+        await this._addDataToCategoryTable();
+      }
     } catch (err) {
       if (err instanceof Error) {
         throw err;
