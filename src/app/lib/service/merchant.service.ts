ca0468ead16ae75ab35cb95737b5b16ed57fe05b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-27 02:17:09 +0100

    feat: add getMerchants

--- src/app/lib/service/merchant.service.ts
+++ src/app/lib/service/merchant.service.ts
@@ -7,14 +7,30 @@ class MerchantService {
 
   async create(data: Merchant) {
     try {
-      await sql`INSERT INTO merchants (name, category_id, website_url, email_address, tags, subtitle, logo_url, post_statue) 
-      VALUES (${data.name}, ${data.category_id}, ${data.website_url}, ${data.email_address}, ${data.tags}, ${data.subtitle}, ${data.logo_url}, ${POST_STATUS_ENUM.DRAFT})`;
+      return await sql`
+      INSERT INTO merchants (merchant_name, category_id, website_url, email_address, tags, subtitle, logo_url, post_status) 
+      VALUES (${data.merchant_name}, ${data.category_id}, ${data.website_url}, ${data.email_address}, ${data.tags}, ${data.subtitle}, ${data.logo_url}, ${POST_STATUS_ENUM.DRAFT})
+      
+      RETURNING true;
+      `;
     } catch (err) {
       if (err instanceof Error) {
         throw err;
       }
 
       throw new Error("MerchantService::create failed");
+    }
+  }
+
+  async getMerchats(): Promise<Merchant[] | []> {
+    try {
+      return await sql`SELECT * FROM merchants`;
+    } catch (err) {
+      if (err instanceof Error) {
+        throw err;
+      }
+
+      throw err;
     }
   }
 }
