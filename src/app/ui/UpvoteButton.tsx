e2d8ffcfcf07e069c910e3f10cef6707e7107223
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-27 02:13:57 +0100

    feat: add UpvoteButton

---
+++ src/app/ui/UpvoteButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { incrementLike } from "../lib/actions";
+
+export default function UpvoteButton({ initialLikes }: { initialLikes: number }) {
+  const [likes, setLikes] = useState(initialLikes);
+
+  return (
+    <>
+      <p>Total Likes: {likes}</p>
+      <button
+        onClick={async () => {
+          const updatedLikes = await incrementLike();
+          setLikes(updatedLikes);
+        }}
+      >
+        Like
+      </button>
+    </>
+  );
+}
