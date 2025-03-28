7fe02e8a0b9f6ee068b418f5ba4b31b0db449283
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-28 08:18:47 +0100

    fix: update DB client

--- src/scripts/database/databaseSeeding.ts
+++ src/scripts/database/databaseSeeding.ts
@@ -1,13 +1,13 @@
-import sql from "../../app/lib/database/sqlConnection";
-import categoriesJson from "./data/category.json";
-import merchantsJson from "./data/squirrel_selected_merchants_with_logo_url_sorted.json";
+// import { PoolClient } from "pg";
+import { dbClient, sql } from "../../app/lib/database/sqlConnection";
+import categoriesJson from "./data/categories_with_id.json";
+import merchantsJson from "./data/updatedMerchantsWithLogoRelativePath_Fri Mar 28 2025 05:18:31 GMT+0100 (West Africa Standard Time).json";
 
 export default class DatabaseSeeding {
-
   async dbConnectionTest() {
     try {
-      const dbTime = await sql`SELECT now()`;
-      console.log("Current time of DB Server: ", dbTime[0], "\n");
+      const result = await sql(`SELECT now()`);
+      console.log("Current time of DB Server: ", result.rows[0], "\n");
     } catch (err) {
       if (err instanceof Error) {
         throw err;
@@ -18,17 +18,17 @@ export default class DatabaseSeeding {
 
   async _createCategoryTable() {
     try {
-      await sql`
+      await sql(`
         DROP TABLE IF EXISTS categories CASCADE;
         CREATE TABLE IF NOT EXISTS categories(
             category_id uuid NOT NULL DEFAULT gen_random_uuid(),
-            name VARCHAR(120) NOT NULL,
+            category_name VARCHAR(120) NOT NULL,
             created_at TIMESTAMP DEFAULT NOW(),
             updated_at TIMESTAMP,
 
             CONSTRAINT category_pkey PRIMARY KEY (category_id)
         );
-      `.simple() /** Executing two statement at once with `.simple()` */;
+      `);
 
       console.log("Categories table created successfully");
       return true;
@@ -42,53 +42,63 @@ export default class DatabaseSeeding {
   }
 
   async _addDataToCategoryTable() {
-    const catObjArr: Record<string, string>[] = [];
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    Object.entries(categoriesJson).map(([_, value]) => {
-      catObjArr.push({ name: value });
-    });
-
-    console.log("Seeding Category table with: \n", catObjArr);
+    const client = await dbClient.connect();
 
     try {
-      await sql`insert into categories ${sql(catObjArr, "name")}`;
+      const queryText = `INSERT INTO categories (category_name) VALUES ($1)`;
+      await client.query("BEGIN");
+
+      // Loop through objArr and add each object to the batch
+      for (const obj of categoriesJson) {
+        console.log("Seeding Category table with: \n", obj.category_name);
+
+        await dbClient.query(queryText, [obj.category_name]);
+      }
+
+      // Commit the txn
+      await client.query("COMMIT");
       console.log("Seeding Category table was successful!");
     } catch (err) {
+      await dbClient.query("ROLLBACK");
+
       if (err instanceof Error) {
+        console.error(err.message);
         throw err;
       }
 
       throw new Error("Seeding Category failed with");
+    } finally {
+      client.release();
     }
   }
 
-  private async _createMerchantsTable() {
+  async createMerchantsTable() {
     try {
-      await sql`
+      await sql(`
           DROP TYPE IF EXISTS post_status_enum CASCADE;
           CREATE TYPE post_status_enum AS ENUM ('publish', 'review', 'draft');
           DROP TABLE IF EXISTS merchants;
+
           CREATE TABLE IF NOT EXISTS merchants(
-          merchant_id uuid NOT NULL DEFAULT gen_random_uuid(),
-          name VARCHAR(50) UNIQUE NOT NULL,
-          category_id uuid NOT NULL,
-          website_url VARCHAR NOT NULL,
-          email_address VARCHAR(100),
-          subtitle VARCHAR(255),
-          description VARCHAR,
-          logo_url VARCHAR(255) NOT NULL,
-          upvote_count INT DEFAULT 0,
-          tags TEXT[],
-          post_status post_status_enum,
-          created_at TIMESTAMP DEFAULT NOW(),
-          updated_at TIMESTAMP,
-          CONSTRAINT merchant_pkey PRIMARY KEY (merchant_id),
-          CONSTRAINT fkey_category FOREIGN KEY (category_id)
-              REFERENCES categories (category_id) 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION
-          );`.simple();
+            merchant_id uuid NOT NULL DEFAULT gen_random_uuid(),
+            merchant_name VARCHAR(50) UNIQUE NOT NULL,
+            category_id uuid NOT NULL,
+            website_url VARCHAR NOT NULL,
+            email_address VARCHAR(100),
+            subtitle VARCHAR(255),
+            description VARCHAR,
+            logo_url VARCHAR(255) NOT NULL,
+            upvote_count INT DEFAULT 0,
+            tags TEXT[],
+            post_status post_status_enum,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP,
+            CONSTRAINT merchant_pkey PRIMARY KEY (merchant_id),
+            CONSTRAINT fkey_category FOREIGN KEY (category_id)
+                REFERENCES categories (category_id) 
+                ON DELETE CASCADE 
+                ON UPDATE NO ACTION
+          )`);
 
       console.log("Creating Merchants table was successful!");
       return true;
@@ -101,40 +111,62 @@ export default class DatabaseSeeding {
     }
   }
 
-  async _seedMerchantTable() {
+  async seedMerchantTable() {
     try {
-      await sql`INSERT INTO merchants ${sql(
-        merchantsJson,
-        "name",
-        "category_id",
-        "website_url",
-        "email_address",
-        "subtitle",
-        "description",
-        "logo_url",
-        "upvote_count",
-        // 'tags',
-        "post_status"
-      )}`;
-      console.log("Seeding Category table was successful!");
+      await dbClient.query("BEGIN");
+
+      const query = `INSERT INTO merchants (merchant_name, category_id,website_url,email_address,subtitle,description,logo_url,upvote_count,post_status,tags)
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`;
+
+      let counter = 0;
+      console.log("Seeding started...");
+      for (const m of merchantsJson) {
+        counter++;
+        const values = [
+          m.merchant_name,
+          m.category_id,
+          m.website_url,
+          m.email_address,
+          m.subtitle,
+          m.description,
+          m.logo_url_relative_path,
+          m.upvote_count,
+          m.post_status,
+          m.tags
+        ];
+
+        try {
+          await dbClient.query(query, values);
+          console.log(`Row seeded: ${counter} : with Merchant: ${m.merchant_name}`);
+        } catch (err) {
+          console.error(`Error inserting merchant ${m.merchant_name}:`, err);
+          throw err; // If one insertion fails, rollback the entire transaction
+        }
+      }
+
+      // Commit the transaction after all inserts
+      await dbClient.query("COMMIT");
+      console.log("\nSeeding Merchant table was successful!");
     } catch (err) {
+      await dbClient.query("ROLLBACK");
+
       if (err instanceof Error) {
         throw err;
       }
-
       throw new Error("_seedMerchantTable failed");
+    } finally {
+      await dbClient.end();
     }
   }
 
   async seedCategoryTable() {
     try {
-      const result = await this._createCategoryTable();
-
-      if (result) {
-        await this._addDataToCategoryTable();
-        await this._createMerchantsTable();
-        await this._seedMerchantTable();
-      }
+      // const result = await this._createCategoryTable();
+      // await this._createMerchantsTable();
+      // await this.seedMerchantTable();
+      // if (result) {
+      // await this._addDataToCategoryTable();
+      // }
     } catch (err) {
       if (err instanceof Error) {
         throw err;
@@ -142,9 +174,5 @@ export default class DatabaseSeeding {
 
       throw Error("Failed to created Categories table");
     }
-  }
-
-  async closeConnection(time_in_seconds: number) {
-    await sql.end({ timeout: time_in_seconds });
   }
 }
