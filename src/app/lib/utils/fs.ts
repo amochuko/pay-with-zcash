3a6943ff80a5bad90cf5d289be28ccfd146fcb76
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-28 08:54:03 +0100

    update: add title case utils

---
+++ src/app/lib/utils/fs.ts
@@ -0,0 +1,45 @@
+import fs from "node:fs";
+import path from "node:path";
+import { Merchant } from "../models/Merchant";
+
+async function saveLogo(imgUrl: string, saveToPath: string): Promise<string> {
+  const saveToPathArr = saveToPath.split("/public");
+
+  try {
+    const response = await fetch(imgUrl);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image. Status: ${response.status}`);
+    }
+
+    const arrBuffer = await response.arrayBuffer();
+
+    return new Promise((res, rej) => {
+      fs.writeFile(saveToPath, Buffer.from(arrBuffer), null, (err) => {
+        if (err) {
+          console.error("Error writing image: ", err);
+          rej(err.message);
+        } else {
+          console.log(`Image saved to `, saveToPath);
+          res(saveToPathArr[saveToPathArr.length - 1]);
+        }
+      });
+    });
+  } catch (err) {
+    console.error("Error fetching or saving image:", err);
+    throw err;
+  }
+}
+
+export async function writeLogoToDisk(data: Merchant) {
+  const logoPathArr = data.logo_url.split("/");
+  const logoFileExt = logoPathArr[logoPathArr.length - 1].split(".")[1];
+  const logoFileName = `${data.merchant_name
+    .toLowerCase()
+    .split(" ")
+    .join("_")}_${Date.now()}.${logoFileExt}`;
+
+  const filePath = path.join(process.cwd(), "public/images", logoFileName);
+  const savedPath = await saveLogo(data.logo_url, filePath);
+  return savedPath;
+}
---
+++ src/app/lib/utils/string.ts
@@ -0,0 +1,6 @@
+export function convertToTitleCase(str: string) {
+  return str
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join(" ");
+}
