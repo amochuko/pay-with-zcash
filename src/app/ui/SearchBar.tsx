7bde76db6310cac17c67f03874e3808320a5ef76
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-28 12:11:26 +0100

    update: add search

---
+++ src/app/ui/SearchBar.tsx
@@ -0,0 +1,12 @@
+export default function SearchBar(){
+    return (
+      <div>
+
+        <input
+          type="text"
+          placeholder="Search..."
+          className="uppercase border border-white/[.45] p-4 max-w-2xl"
+        />
+      </div>
+    );
+}
