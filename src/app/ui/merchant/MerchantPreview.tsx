84dc1169c919befe8bca664fe7e9557f87fbe3a9
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-28 14:26:18 +0100

    update: add props

--- src/app/ui/merchant/MerchantPreview.tsx
+++ src/app/ui/merchant/MerchantPreview.tsx
@@ -1,81 +1,72 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { getMerchants, incrementLike } from "../../lib/actions";
+import { convertToTitleCase } from "@/app/lib/utils/string";
+import Image from "next/image";
 import { Merchant } from "../../lib/models/Merchant";
 
-export default function MerchantPreview() {
-  const [merchantList, setMerchantList] = useState<Merchant[]>([]);
+type MerchantPreviewProps = {
+  isOpen: boolean;
+  merchant: Merchant;
+  onClose: () => void;
+};
+export default function MerchantPreview(props: MerchantPreviewProps) {
+  if (!props.isOpen) return null;
 
-  useEffect(() => {});
-
-  const upvote = async () => {
-    await incrementLike();
-  };
-
-  const getAllMerchants = async () => {
-    try {
-      const m = await getMerchants();
-
-      console.log({ merchant: m });
-    } catch (err) {
-      console.error("Error 101: ", err);
-    }
-  };
+  const description =
+    props.merchant.description ||
+    props.merchant.subtitle ||
+    "This merchant has no description.";
 
   return (
-    <div className="flex flex-col bg-white p-6 rounded-lg shadow-lg w-full max-w-xs md:max-w-sm lg:max-w-md">
-      {/* <!-- Logo on the left --> */}
-      <div className="flex items-center mb-4">
-        {/* <Image
-              src="path/to/logo.png"
-              alt="Logo"
-              className="w-16 h-16 mr-4"
-              width={100}
-              height={100}
-            /> */}
-        <h3 className="text-2xl font-semibold text-gray-800">Product Name</h3>
+    <div
+      className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50"
+      onClick={props.onClose}
+    >
+      <div
+        className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full relative"
+        onClick={(e) => e.stopPropagation()} // Prevent close when clicking inside modal
+      >
+        <button
+          onClick={props.onClose}
+          className="absolute top-2 right-2 bg-[#F4B728] text-black text-2xl rounded-full w-12 h-12 p-2 cursor-pointer font-extrabold"
+        >
+          ×
+        </button>
+        <h3 className="text-2xl font-semibold text-center my-4 text-slate-700">
+          {props.merchant.merchant_name}
+        </h3>
+        <p className="text-gray-800 text-center mb-4">
+          {convertToTitleCase(description)}
+        </p>
+        <Image
+          width={120}
+          height={120}
+          src={props.merchant.logo_url}
+          alt="Example"
+          className="w-full h-auto rounded mb-4"
+        />
+        {/* <!-- List of Tags --> */}
+        <div className="flex flex-wrap space-x-2 mb-4">
+          {props.merchant.tags && props.merchant.tags.length > 0
+            ? props.merchant.tags.map((tag, i) => (
+                <span
+                  key={tag + "_" + i}
+                  className="bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm mb-2"
+                >
+                  {tag}
+                </span>
+              ))
+            : null}
+        </div>
+        <a
+          href={props.merchant.website_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline text-center block"
+        >
+          Visit Merchant Website
+        </a>
       </div>
-
-      {/* <!-- Additional text - Red Rock Coffeehouse --> */}
-      <p className="text-lg text-gray-600 mb-4">Red Rock Coffeehouse</p>
-
-      {/* <!-- Upvote Button --> */}
-      <button
-        onClick={upvote}
-        className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none mb-4 cursor-pointer"
-      >
-        Upvote
-      </button>
-
-      <button
-        onClick={getAllMerchants}
-        className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none mb-4 cursor-pointer"
-      >
-        Get Merchants
-      </button>
-
-      {/* <!-- List of Tags --> */}
-      <div className="flex flex-wrap space-x-2 mb-4">
-        <span className="bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm mb-2">
-          Tag 1
-        </span>
-        <span className="bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm mb-2">
-          Tag 2
-        </span>
-        <span className="bg-gray-200 text-gray-700 py-1 px-3 rounded-full text-sm mb-2">
-          Tag 3
-        </span>
-      </div>
-
-      {/* <!-- External Website Link --> */}
-      <a
-        href="https://www.example.com"
-        target="_blank"
-        className="text-blue-500 hover:text-blue-700 text-sm"
-      >
-        Visit Website
-      </a>
     </div>
   );
 }
