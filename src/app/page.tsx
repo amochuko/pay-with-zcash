dfcbce062cd1c2d1ec5f356c8037304141e804e0
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-29 04:15:02 +0100

    update: client comp to state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
 import { getAllCategory, getMerchants } from "./lib/actions";
-import MerchantList from "./ui/merchant/MerchantList";
-import SearchBar from "./ui/SearchBar";
+import SearchableHomePage from "./ui/SearchableHomePage";
 
 export default async function HomePage() {
   const merchants = await getMerchants();
@@ -9,21 +7,7 @@ export default async function HomePage() {
 
   return (
     <div className="min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col ">
-      <header className="flex flex-col my-24 gap-8">
-        <h1 className="text-2xl">Discover Where to Pay With Zcash</h1>
-        <div className="flex justify-between">
-          <SearchBar />
-          <Link
-            href={"/submit-listing"}
-            className="uppercase border border-white/[.45] p-4"
-          >
-            Add your business
-          </Link>
-        </div>
-      </header>
-      <main className="flex flex-wrap gap-[32px] items-center justify-center sm:items-start">
-        <MerchantList merchants={merchants} categories={categories} />
-      </main>
+      <SearchableHomePage categories={categories} merchants={merchants} />
     </div>
   );
 }
