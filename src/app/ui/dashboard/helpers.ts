8169a8a7ef7d51ded951243f9ba12704238094bb
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 17:00:14 +0100

    fix: add pagination

--- src/app/ui/dashboard/helpers.ts
+++ src/app/ui/dashboard/helpers.ts
@@ -11,3 +11,15 @@ export function parseCategoryInMerchants(
     return { ...m, categoryName: category?.category_name };
   });
 }
+
+type List<T> = T[];
+
+export const paginateArrayItems = <T>(
+  list: List<T>,
+  currentPage: number,
+  itemsPerPage: number
+): List<T> => {
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  return list.slice(indexOfFirstItem, indexOfLastItem);
+};
