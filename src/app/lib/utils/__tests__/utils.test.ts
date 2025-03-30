740b639c2942530a58162fa878b3f7531ebfed86
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 12:49:40 +0100

    update: add test for utils functions

--- src/app/lib/utils/__tests__/utils.test.ts
+++ src/app/lib/utils/__tests__/utils.test.ts
@@ -1,4 +1,8 @@
-import { convertToTitleCase } from "../string";
+import {
+  convertToTitleCase,
+  formatDateToHumanReadable,
+  trimText,
+} from "../string";
 
 describe("Utils", () => {
   it("should return a Title Case of the str", () => {
@@ -6,5 +10,21 @@ describe("Utils", () => {
     const str = convertToTitleCase(txt);
 
     expect(str).toBe("This Is It");
+  });
+
+  it("should return a Text of specified length", () => {
+    const txt = "Karen Harrod is an artist and the time it takes to create";
+    const result = trimText(txt, 8);
+
+    expect(result).toBe("Karen Ha...");
+  });
+
+  it("should return date as March, 27, 2025", () => {
+    const date =
+      "Thu Mar 27 2025 16:02:51 GMT+0100 (West Africa Standard Time)";
+    const expected = "March 27, 2025";
+    const result = formatDateToHumanReadable(date);
+
+    expect(result).toBe(expected);
   });
 });
