92d4506be079b078e8f1e9fee71df880cd151ddd
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 11:59:34 +0100

    update: add trimText

--- src/app/lib/utils/string.ts
+++ src/app/lib/utils/string.ts
@@ -4,3 +4,7 @@ export function convertToTitleCase(str: string) {
     .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
     .join(" ");
 }
+
+export function trimText(txt: string, length: number) {
+  return txt && txt.slice(0, length) + "...";
+}
