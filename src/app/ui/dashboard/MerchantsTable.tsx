1449522076c4f7be9bf2bdaad40a9f0b2c996995
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 12:20:27 +0100

    update: add MerchantsTable comp

---
+++ src/app/ui/dashboard/MerchantsTable.tsx
@@ -0,0 +1,70 @@
+'use client'
+import { Merchant } from "@/app/lib/models/Merchant";
+import { POST_STATUS_ENUM } from "@/app/lib/typings";
+import { convertToTitleCase, trimText } from "@/app/lib/utils/string";
+import Tags from "../Tags";
+
+
+type MerchantsTableProps = {
+  merchants: Merchant[]
+};
+
+const MerchantsTable = (props: MerchantsTableProps) => {
+    return (
+      <>
+        {/* Merchants Table */}
+        <div className="bg-slate-700 rounded-lg shadow-md p-6">
+          <h2 className="text-xl font-semibold mb-4">Merchants</h2>
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="bg-slate-900">
+                <th className="px-4 py-2 text-left">S/N</th>
+                <th className="px-4 py-2 text-left">Name</th>
+                <th className="px-4 py-2 text-left">Description</th>
+                <th className="px-4 py-2 text-left">Tags</th>
+                <th className="px-4 py-2 text-left">Category</th>
+                <th className="px-4 py-2 text-left">Status</th>
+                <th className="px-4 py-2 text-left">Date Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.merchants &&
+                props.merchants.slice(0, 8).map((merchant, i) => (
+                  <tr key={merchant.category_id + i}>
+                    <td className="px-4 py-2">{i + 1}</td>
+                    <td className="px-4 py-2">
+                      {convertToTitleCase(merchant.merchant_name)}
+                    </td>
+                    <td className="px-4 py-2">
+                      {trimText(merchant.subtitle, 30) ||
+                        trimText(merchant.description, 30)}
+                    </td>
+                    <td className="px-4 py-2">
+                      <Tags tags={merchant.tags} />
+                    </td>
+                    <td className="px-4 py-2">{merchant.category_id}</td>
+                    <td className="px-4 py-2">
+                      <span
+                        className={`${
+                          merchant.post_status === POST_STATUS_ENUM.PUBLISH
+                            ? "bg-green-200 text-green-800"
+                            : "bg-yellow-200 text-yellow-800"
+                        } px-2 py-1 rounded`}
+                      >
+                        {merchant.post_status}
+                      </span>
+                    </td>
+                    <td className="px-4 py-2">
+                      {merchant.created_at?.toString()}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+}
+
+
+export default MerchantsTable;
