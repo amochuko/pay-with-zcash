e93bfb1ab7df04f8155d30ccefb98c3f279823b0
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 15:41:10 +0100

    fix: update category_id to category_name

--- src/app/ui/dashboard/MerchantsTable.tsx
+++ src/app/ui/dashboard/MerchantsTable.tsx
@@ -7,67 +7,73 @@ import {
   trimText,
 } from "@/app/lib/utils/string";
 import Tags from "../Tags";
+import { CategoriesTableProps } from "./CategoriesTable";
 
 type MerchantsTableProps = {
   merchants: Merchant[];
-};
+} & CategoriesTableProps;
 
 const MerchantsTable = (props: MerchantsTableProps) => {
+  const merchants = props.merchants.map((m) => {
+    const category = props.categories.find(
+      (c) => c.category_id == m.category_id
+    );
+
+    return { ...m, categoryName: category?.category_name };
+  });
+
   return (
-    <>
-      {/* Merchants Table */}
-      <div className="bg-slate-700 rounded-lg shadow-md p-6">
-        <h2 className="text-xl font-semibold mb-4">Merchants</h2>
-        <div className="overflow-x-auto">
-          <table className="w-full table-auto">
-            <thead>
-              <tr className="bg-slate-900">
-                <th className="px-4 py-2 text-left">S/N</th>
-                <th className="px-4 py-2 text-left">Name</th>
-                <th className="px-4 py-2 text-left">Description</th>
-                <th className="px-4 py-2 text-left">Tags</th>
-                <th className="px-4 py-2 text-left">Category</th>
-                <th className="px-4 py-2 text-left">Status</th>
-                <th className="px-4 py-2 text-left">Date Created</th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.merchants &&
-                props.merchants.slice(0, 8).map((merchant, i) => (
-                  <tr key={merchant.category_id + i}>
-                    <td className="px-4 py-2">{i + 1}</td>
-                    <td className="px-4 py-2">
-                      {convertToTitleCase(merchant.merchant_name)}
-                    </td>
-                    <td className="px-4 py-2">
-                      {trimText(merchant.subtitle, 30) ||
-                        trimText(merchant.description, 30)}
-                    </td>
-                    <td className="px-4 py-2">
-                      <Tags tags={merchant.tags} />
-                    </td>
-                    <td className="px-4 py-2">{merchant.category_id}</td>
-                    <td className="px-4 py-2">
-                      <span
-                        className={`${
-                          merchant.post_status === POST_STATUS_ENUM.PUBLISH
-                            ? "bg-green-200 text-green-800"
-                            : "bg-yellow-200 text-yellow-800"
-                        } px-2 py-1 rounded`}
-                      >
-                        {merchant.post_status}
-                      </span>
-                    </td>
-                    <td className="px-4 py-2">
-                      {formatDateToHumanReadable(String(merchant.created_at))}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
+    <div className="bg-slate-700 rounded-lg shadow-md p-6">
+      <h2 className="text-xl font-semibold mb-4">Merchants</h2>
+      <div className="overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-slate-900">
+              <th className="px-4 py-2 text-left">S/N</th>
+              <th className="px-4 py-2 text-left">Name</th>
+              <th className="px-4 py-2 text-left">Description</th>
+              <th className="px-4 py-2 text-left">Tags</th>
+              <th className="px-4 py-2 text-left">Category</th>
+              <th className="px-4 py-2 text-left">Status</th>
+              <th className="px-4 py-2 text-left">Date Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {merchants &&
+              merchants.slice(0, 8).map((merchant, i) => (
+                <tr key={merchant.category_id + i}>
+                  <td className="px-4 py-2">{i + 1}</td>
+                  <td className="px-4 py-2">
+                    {convertToTitleCase(merchant.merchant_name)}
+                  </td>
+                  <td className="px-4 py-2">
+                    {trimText(merchant.subtitle, 30) ||
+                      trimText(merchant.description, 30)}
+                  </td>
+                  <td className="px-4 py-2">
+                    <Tags tags={merchant.tags} />
+                  </td>
+                  <td className="px-4 py-2">{merchant.categoryName}</td>
+                  <td className="px-4 py-2">
+                    <span
+                      className={`${
+                        merchant.post_status === POST_STATUS_ENUM.PUBLISH
+                          ? "bg-green-200 text-green-800"
+                          : "bg-yellow-200 text-yellow-800"
+                      } px-2 py-1 rounded`}
+                    >
+                      {merchant.post_status}
+                    </span>
+                  </td>
+                  <td className="px-4 py-2">
+                    {formatDateToHumanReadable(String(merchant.created_at))}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
   );
 };
 
