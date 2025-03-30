a929c60175a4abfa5c4dcafe036e78123f9ccd8f
amochuko <7204868+amochuko@users.noreply.github.com>
2025-03-30 12:11:43 +0100

    update: Add Tags component

---
+++ src/app/ui/Tags.tsx
@@ -0,0 +1,22 @@
+type TagsProps = {
+  tags: string[];
+};
+const Tags = (props: TagsProps) => {
+  return (
+    <div className="flex flex-wrap space-x-2 mb-4">
+      {/* <!-- List of Tags --> */}
+      {props.tags && props.tags.length > 0
+        ? props.tags.map((tag, i) => (
+            <span
+              key={tag + "_" + i}
+              className="bg-gray-300 text-gray-900 py-1 px-3 rounded-full text-sm mb-2"
+            >
+              {tag}
+            </span>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default Tags;
