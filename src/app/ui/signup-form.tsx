caa5182aad59ab08f0974431d1fb0a383fb4cb7d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-01 14:09:24 +0100

    fix: update css

--- src/app/ui/signup-form.tsx
+++ src/app/ui/signup-form.tsx
@@ -27,7 +27,7 @@ export default function SignupForm() {
           className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
         />
         {state?.errors?.name && (
-          <p className="text-red-500 text-sm mt-1">{state.errors.name}</p>
+          <p className="text-red-400 mt-2">{state.errors.name}</p>
         )}
       </div>
 
@@ -47,7 +47,7 @@ export default function SignupForm() {
           className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
         />
         {state?.errors?.email && (
-          <p className="text-red-500 text-sm mt-1">{state.errors.email}</p>
+          <p className="text-red-400 mt-2">{state.errors.email}</p>
         )}
       </div>
 
@@ -67,8 +67,8 @@ export default function SignupForm() {
           className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
         />
         {state?.errors?.password && (
-          <div className="text-red-500 text-sm mt-2">
-            <p>Password must:</p>
+          <div className="text-red-400 mt-2">
+            <p className="mb-1">Password must:</p>
             <ul className="list-disc pl-5 space-y-1">
               {state.errors.password.map((err, i) => (
                 <li key={err + "_" + i}>{err}</li>
@@ -83,7 +83,7 @@ export default function SignupForm() {
         disabled={pending}
         className="w-full py-4 text-lgpx-4 mt-8 bg-[#F4B728] text-black font-medium rounded-md hover:bg-[#CF9B20] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:bg-gray-400 cursor-pointer"
       >
-        Sign Up
+        {pending ? "Processing..." : "Sign Up"}
       </button>
     </form>
   );
