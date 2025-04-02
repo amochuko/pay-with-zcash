337cce522eca9ce54faeb0832ec55db38b23bc04
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 08:30:04 +0100

    update: add login and logout

--- src/app/actions/auth.action.ts
+++ src/app/actions/auth.action.ts
@@ -1,7 +1,13 @@
-'use server'
+"use server";
+import { signIn, signOut } from "@/auth";
 import bcrypt from "bcryptjs";
-import { SignupFormSchema, SignupFormState } from "../lib/typings";
+import { redirect } from "next/navigation";
 import authService from "../lib/service/auth.service";
+import {
+  LoginStateForm,
+  SignupFormSchema,
+  SignupFormState,
+} from "../lib/typings";
 
 export async function signUp(state: SignupFormState, formData: FormData) {
   const validatedFields = SignupFormSchema.safeParse({
@@ -19,18 +25,76 @@ export async function signUp(state: SignupFormState, formData: FormData) {
   const { email, name, password } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  console.log({ email, name, password, hashedPassword });
-
   const result = await authService.signUp({ email, name, hashedPassword });
-  const user = result?.row;
+  const user = result;
 
   if (!user) {
     return {
       message: "An error occurred while creating your account.",
     };
+  }
 
-    // TODO:
-    // Create user session
-    // Redirect user
+  // TODO:
+  // Create user session
+  // Redirect user
+
+  console.log({ user });
+  redirect("/login");
+}
+
+export async function logIn(state: LoginStateForm, formData: FormData) {
+  const validatedFields = SignupFormSchema.omit({ name: true }).safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    const { email, password } = validatedFields.data;
+    console.log({ email, password });
+
+    // const user = await authService.findUserByEmail(email);
+
+    // if (!user) {
+    //   return {
+    //     message: "An error occurred with the login process",
+    //   };
+    // }
+    // const confirmPassword = await bcrypt.compare(
+    //   password,
+    //   String(user.password)
+    // );
+
+    // console.log({ confirmPassword });
+
+    // return user;
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
+}
+
+export async function login() {
+  try {
+    await signIn("github", {
+      redirect: "/dashboard",
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function logout(authType: string, formData: FormData) {
+  console.log({ authType, formData });
+  try {
+    await signOut();
+  } catch (err) {
+    console.error(err);
   }
 }
