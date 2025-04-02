8470efdac1327641cc9ce9814a3f07eee64033d5
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 08:27:30 +0100

    fix: update with role prop

--- src/app/lib/models/User.ts
+++ src/app/lib/models/User.ts
@@ -2,4 +2,9 @@ export default class User {
   readonly name: string | undefined;
   email: string | undefined;
   password: string | undefined;
+  role: Role | undefined;
 }
+
+export type Role = {
+  admin: string;
+};
