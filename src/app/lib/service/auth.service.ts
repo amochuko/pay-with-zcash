e2d345ae3cb1d64560bb02e72e33a1eeb298b3ab
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 08:27:04 +0100

    update: add findByEmail

--- src/app/lib/service/auth.service.ts
+++ src/app/lib/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
+import { sql } from "../database/sqlConnection";
 import { SignupFormSchema } from "../typings";
-import {sql} from '../database/sqlConnection'
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const SignupArgs = SignupFormSchema.omit({ password: true });
 type SignupArgsType = z.infer<typeof SignupArgs> & { hashedPassword: string };
@@ -8,8 +9,6 @@ type SignupArgsType = z.infer<typeof SignupArgs> & { hashedPassword: string };
 class AuthService {
   async signUp(args: SignupArgsType) {
     try {
-
-
       const result = await sql(
         `INSERT INTO users (name,email,password)
         VALUES ($1,$2,$3)
@@ -18,8 +17,27 @@ class AuthService {
       );
 
       if (result.rowCount === 1) {
-        return result;
+        return result.rows[0].user_id;
       }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async findUserByEmail(email: string, password: string) {
+    try {
+      const result = await sql(
+        `
+          SELECT email * users
+          WHERE email = ($1) AND password = ($2)`,
+        [email, password]
+      );
+
+      if (result.rowCount === 0) {
+        return `Account does not exist.`;
+      }
+
+      return result.rows[0];
     } catch (err) {
       throw err;
     }
