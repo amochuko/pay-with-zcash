9fd9b7ebe901445da394d0aefa4a7f1b5a37cbe5
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 14:54:59 +0100

    update: add config for github

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [],
+  providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID || "",
+      clientSecret: process.env.AUTH_GITHUB_SECRET || "",
+      name: "Pay With Zcash",
+    }),
+  ],
 });
