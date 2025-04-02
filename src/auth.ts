f01407bb9e1c0bbc5aefc7c37e5eeea1d4198b78
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 09:14:53 +0100

    fix: remove provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
 
-
-export const {auth, handlers, signIn, signOut} = NextAuth({
-    providers:[Github({})]
-})
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [],
+});
