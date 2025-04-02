49451bf1fbd720a29034572698aa748e79cdf450
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-02 15:00:32 +0100

    fix: add access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,39 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { SESSION_PAY_WITH_ZCASH } from "./app/lib/config";
+import { verifyJwt } from "./app/lib/session";
 
-export function middleware(req: NextRequest) {
-  console.log({ req: req.url });
+const publicRoutes = ["/login"];
 
-  // if (req.nextUrl.pathname.startsWith("/dashboard")) {
-  //   return NextResponse.rewrite(new URL("/dashboard-admin", req.url));
-  // }
+export async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = path.startsWith("/dashboard");
+  const isPublicRoute = publicRoutes.includes(path);
 
-  return NextResponse.redirect(new URL("/find-an-atm", req.url));
+  const cookieStore = await cookies();
+  const cookie = String(cookieStore.get(SESSION_PAY_WITH_ZCASH)?.value);
+
+  const session = await verifyJwt(cookie);
+
+  if (isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
   // Matcher is used to specify which route(s) the middleware is executed on
   // Executes the above function whenever this list of urlPath is reached
-  matcher: ["/about/:path"],
+  matcher: [
+    "/dashboard",
+    "/login",
+    "/dashboard/categories",
+    "/dashboard/merchants",
+    "/dashboard/settings",
+  ],
 };
