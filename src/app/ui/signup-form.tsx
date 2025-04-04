c0e47ef7d7054d37e029eb5df34fd53ebf2915b1
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-04 03:44:28 +0100

    fix: update with accessibility

--- src/app/ui/signup-form.tsx
+++ src/app/ui/signup-form.tsx
@@ -27,7 +27,9 @@ export default function SignupForm() {
           className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
         />
         {state?.errors?.name && (
-          <p className="text-red-400 mt-2">{state.errors.name}</p>
+          <p aria-live="polite" className="text-red-400 mt-2">
+            {state.errors.name}
+          </p>
         )}
       </div>
 
@@ -47,7 +49,9 @@ export default function SignupForm() {
           className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
         />
         {state?.errors?.email && (
-          <p className="text-red-400 mt-2">{state.errors.email}</p>
+          <p aria-live="polite" className="text-red-400 mt-2">
+            {state.errors.email}
+          </p>
         )}
       </div>
 
@@ -68,7 +72,9 @@ export default function SignupForm() {
         />
         {state?.errors?.password && (
           <div className="text-red-400 mt-2">
-            <p className="mb-1">Password must:</p>
+            <p aria-live="polite" className="mb-1">
+              Password must:
+            </p>
             <ul className="list-disc pl-5 space-y-1">
               {state.errors.password.map((err, i) => (
                 <li key={err + "_" + i}>{err}</li>
@@ -81,10 +87,18 @@ export default function SignupForm() {
       <button
         type="submit"
         disabled={pending}
-        className="w-full py-4 text-lgpx-4 mt-8 bg-[#F4B728] text-black font-medium rounded-md hover:bg-[#CF9B20] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:bg-gray-400 cursor-pointer"
+        className="w-full py-4 text-lgpx-4 my-8 bg-[#F4B728] text-black font-medium rounded-md hover:bg-[#CF9B20] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:bg-gray-400 cursor-pointer"
       >
         {pending ? "Processing..." : "Sign Up"}
       </button>
+
+      {state?.message && (
+        <div className="text-red-400 mt-4">
+          <p aria-live="polite" className="mb-1">
+            {state.message}
+          </p>
+        </div>
+      )}
     </form>
   );
 }
