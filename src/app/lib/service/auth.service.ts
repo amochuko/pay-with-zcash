a9a6a102895e270d3d10ece7130a88ae178c2de2
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-04 03:43:05 +0100

    fix: update error handling for signup

--- src/app/lib/service/auth.service.ts
+++ src/app/lib/service/auth.service.ts
@@ -7,29 +7,75 @@ const SignupArgs = SignupFormSchema.omit({ password: true });
 type SignupArgsType = z.infer<typeof SignupArgs> & { hashedPassword: string };
 
 class AuthService {
+  //
+
   async signUp(args: SignupArgsType) {
+    console.log({ args });
+
+    if (args.email.length > 150) {
+      throw new Error(
+        "Email address is too long. Maximum allowed length is 150 characters."
+      );
+    }
+
     try {
       const result = await sql(
-        `INSERT INTO users (name,email,password)
-        VALUES ($1,$2,$3)
+        `INSERT INTO accounts (username, email_address, password)
+        VALUES ($1, $2, $3)
         RETURNING *`,
         [args.name, args.email, args.hashedPassword]
       );
 
       if (result.rowCount === 1) {
         return result.rows[0].user_id;
+      } else {
+        throw new Error("Sign up failed. Please try agian.");
       }
     } catch (err) {
-      throw err;
+      if (err instanceof Error) {
+        console.error("Error during sign up:", err.message); // Log the error in a general manner
+
+        // Check for the 'value too long' error (specific to PostgreSQL)
+        if (
+          err.message &&
+          err.message.includes("value too long for type character varying")
+        ) {
+          // Return a generic error message or a more specific one based on the app's context
+          throw new Error(
+            "The provided email address is too long. Please check your input."
+          );
+        }
+
+        // Handle unique constraint violations (if email or username already exists)
+        if (
+          err.message.includes("duplicate key value violates unique constraint")
+        ) {
+          if (err.message.includes("accounts_username_key")) {
+            throw new Error(
+              "Username already exists. Please choose another one."
+            );
+          }
+          if (err.message.includes("accounts_email_address_key")) {
+            throw new Error(
+              "Email address already in use. Please choose another one."
+            );
+          }
+        }
+      }
+
+      // Catch any other errors and return a generic error message
+      throw new Error(
+        "An error occurred during the sign up process. Please try again."
+      );
     }
   }
 
-  async findUserByEmail(email: string) { 
+  async findUserByEmail(email: string) {
     try {
       const result = await sql(
         `
-          SELECT * FROM users
-          WHERE email = ($1)`,
+          SELECT * FROM accounts
+          WHERE email_address = ($1)`,
         [email]
       );
 
