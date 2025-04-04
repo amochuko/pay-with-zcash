5f0b8233129062c17ad559262d5e69e862d73135
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-04 11:52:53 +0100

    fix: update env to config

--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -1,1 +1,12 @@
-export const SESSION_PAY_WITH_ZCASH = process.env.SESSION_PAY_WITH_ZCASH || 'pay_with_zcash'
+// import dotenv from "dotenv";
+// dotenv.config();
+
+export const SESSION_PAY_WITH_ZCASH =
+  process.env.SESSION_PAY_WITH_ZCASH || "pay_with_zcash";
+export const PGHOST_LOCAL = process.env.PGHOST_LOCAL;
+export const PGUSER_LOCAL = process.env.PGUSER_LOCAL;
+export const PGDATABASE_LOCAL = process.env.PGDATABASE_LOCAL;
+export const PGPASSWORD_LOCAL = process.env.PGPASSWORD_LOCAL;
+export const PGPORT_LOCAL = Number(process.env.PGPORT_LOCAL);
+
+export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
