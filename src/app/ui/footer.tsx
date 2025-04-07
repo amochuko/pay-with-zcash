75a67704bc8299ae9179f871886d204c10ef5611
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-07 11:55:35 +0100

    fix: move footer

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="footer text-center mb-4">
+      <ul className="flex flex-col justify-center gap-1">
+        <li>
+          ©{new Date().getFullYear()} Pay With Zcash
+        </li>
+        <li >
+          <Link className="text-white hover:text-[#FFB400] hover:underline" href="/privacy-policy">Privacy Policy</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+}
