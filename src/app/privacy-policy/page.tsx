dd52e15cdbbfb986c1070225dbb76f618a10a837
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-07 11:13:10 +0100

    upate: add PrivacyPolicy page to dashboard

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+import PrivacyPolicy from "../ui/dashboard/privacy-policy/PrivacyPolicy";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi earum eius rem vero eos expedita blanditiis atque aut sit! Repudiandae necessitatibus obcaecati aperiam praesentium facilis eum quasi error placeat voluptate!
+      </p>
+
+      <PrivacyPolicy />
+    </div>
+  );
+}
