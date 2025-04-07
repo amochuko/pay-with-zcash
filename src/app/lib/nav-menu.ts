13424b701a7d8aea5bf408bdbf3c4a831e2429c7
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-07 07:41:13 +0100

    fix: update string method

--- src/app/lib/nav-menu.ts
+++ src/app/lib/nav-menu.ts
@@ -1,11 +1,11 @@
 const basePathBack = "dashboard";
-const navItemsBackend = ["categories", "merchants", "settings"];
+const navItemsBackend = ["categories", "merchants", "settings", 'privacy policy'];
 
 export const navMenuBackend = navItemsBackend.map((n) => {
   const obj = { title: "", link: "" };
 
   obj["title"] = n;
-  obj["link"] = `${basePathBack}/${n}`;
+  obj["link"] = `${basePathBack}/${n.replace(' ', '-')}`;
 
   return obj;
 });
