330ed5df41aaaaa05cc21c8112cb4001b10bc564
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-07 11:15:44 +0100

    upate: add getPolicies()

--- src/app/lib/service/privacy-policy.service.ts
+++ src/app/lib/service/privacy-policy.service.ts
@@ -29,6 +29,25 @@ class PrivacyPolicyService {
     }
   }
 
+  async getPolicies(): Promise<PrivacyPolicy[]> {
+    try {
+      const res = await sql(`SELECT * FROM public.privacy_policcy AS p
+                        ORDER BY p.serial_num ASC;`);
+
+      if (res.rowCount && res.rowCount > 1) {
+        return res.rows;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        throw err;
+      }
+
+      throw new Error("Failed to fetch Policy list");
+    }
+  }
+
 
 }
 
