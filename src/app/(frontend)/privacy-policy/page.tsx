f5ad0e1848ff7f268e0237352872ab9e00145780
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-07 11:57:02 +0100

    fix: move to group

---
+++ src/app/(frontend)/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicy from "../../ui/dashboard/privacy-policy/PrivacyPolicy";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <>
+      <PrivacyPolicy />
+    </>
+  );
+}
