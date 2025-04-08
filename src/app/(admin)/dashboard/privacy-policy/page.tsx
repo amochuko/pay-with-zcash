0292e6edf5dbc48f9b7ecbe805ff6348297fa63b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-08 05:31:06 +0100

    update: add getPolicies() action

--- src/app/(admin)/dashboard/privacy-policy/page.tsx
+++ src/app/(admin)/dashboard/privacy-policy/page.tsx
@@ -1,5 +1,13 @@
-import PrivacyPolicy from "@/app/privacy-policy/page";
+import { getPolicies } from "@/app/actions/privayc-policy.action";
+import PrivacyPolicy from "@/app/lib/models/PrivacyPolicy";
+import PrivacyPolicyList from "@/app/ui/dashboard/privacy-policy/PrivatePolicyList";
 
-export default function PrivacyPolicyPage() {
-  return <PrivacyPolicy />;
+export default async function PrivacyPolicyPage() {
+  const policies = await getPolicies();
+
+  return (
+    <div className="privacy-policy-dashboard">
+      <PrivacyPolicyList policies={policies.data as PrivacyPolicy[]} />
+    </div>
+  );
 }
