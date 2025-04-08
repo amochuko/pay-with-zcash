862d912741455882cc2cbf6ac607caad331b4797
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-08 05:33:39 +0100

    update: add createPolicy() action

--- src/app/actions/privayc-policy.action.ts
+++ src/app/actions/privayc-policy.action.ts
@@ -22,3 +22,40 @@ export async function getPolicies() {
     return { message: "An unexpected error occurred.", data: undefined };
   }
 }
+
+export async function createPolicy(prevState: unknown, formData: FormData) {
+  const validatedFields = PrivacyPolicySchema.pick({
+    title: true,
+    description: true,
+  }).safeParse(Object.fromEntries(formData));
+
+  if (!validatedFields.success) {
+    return {
+      message: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    const result = await privacyPolicyService.create(validatedFields.data);
+
+    if (result.rowCount === 1) {
+      revalidatePath("/dashboard/privacy-policy");
+      return { data: result.rows[0], message: undefined };
+    }
+
+    return {
+      message: "Some went wrong during merchant creation",
+      data: undefined,
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    if (err.code === "23505") {
+      return {
+        message: "A merchant with this name already exists.",
+        data: undefined,
+      };
+    }
+
+    return { message: "An unexpected error occurred.", data: undefined };
+  }
+}
