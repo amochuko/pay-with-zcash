a50a769ce522e71aa56a7a86c50a782a6ee33e0b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-09 00:30:26 +0100

    update: add deleteById

--- src/app/actions/privacy-policy.action.ts
+++ src/app/actions/privacy-policy.action.ts
@@ -13,9 +13,10 @@ export async function getPolicies(): Promise<{
     const result = await privacyPolicyService.getPolicies();
 
     if (result.length > 0) {
-      revalidatePath("/dashboard/privacy-policy");
       return { data: result, message: "" };
     }
+    
+    revalidatePath("/dashboard/privacy-policy");
 
     return {
       message: "No policies available.",
@@ -45,7 +46,7 @@ export async function createPolicy(prevState: unknown, formData: FormData) {
   try {
     const result = await privacyPolicyService.create(validatedFields.data);
 
-    if (result.rowCount === 1) {
+    if (result && result.rowCount === 1) {
       revalidatePath("/dashboard/privacy-policy");
       return { data: result.rows[0], message: "" };
     }
@@ -55,11 +56,48 @@ export async function createPolicy(prevState: unknown, formData: FormData) {
       message: "Something went wrong during policy creation",
     };
   } catch (err) {
-    console.error("Error creating Policy: ", err);
-
     return {
       data: [],
-      message: "An unexpected error occurred.",
+      message: String(err),
     };
   }
 }
+
+export async function deleteById(policyId: string) {
+  console.log({ policyId });
+
+  const validatedFields = PrivacyPolicySchema.pick({
+    id: true,
+  }).safeParse(policyId);
+
+  console.log(validatedFields.error);
+
+  if (!validatedFields.success) {
+    return {
+      data: [],
+      message: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    const result = await privacyPolicyService.deleteById(
+      validatedFields.data.id
+    );
+
+    if (result && result.rowCount === 1) {
+      revalidatePath("/dashboard/privacy-policy");
+      return { data: result.rows[0], message: "" };
+    }
+
+    return {
+      data: [],
+      message: "Something went wrong during policy deletion",
+    };
+  } catch (err) {
+    return {
+      data: [],
+      message: String(err),
+    };
+  }
+}
+
