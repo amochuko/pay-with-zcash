4bb99d9f9b2b06685fca0d285e4f0a4c8e0b125c
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-08 09:26:43 +0100

    fix: update props

--- src/app/(admin)/dashboard/privacy-policy/page.tsx
+++ src/app/(admin)/dashboard/privacy-policy/page.tsx
@@ -1,13 +1,12 @@
-import { getPolicies } from "@/app/actions/privayc-policy.action";
-import PrivacyPolicy from "@/app/lib/models/PrivacyPolicy";
+import { getPolicies } from "@/app/actions/privacy-policy.action";
 import PrivacyPolicyList from "@/app/ui/dashboard/privacy-policy/PrivatePolicyList";
 
 export default async function PrivacyPolicyPage() {
-  const policies = await getPolicies();
+  const result = await getPolicies();
 
   return (
     <div className="privacy-policy-dashboard">
-      <PrivacyPolicyList policies={policies.data as PrivacyPolicy[]} />
+      <PrivacyPolicyList policyObj={result} />
     </div>
   );
 }
