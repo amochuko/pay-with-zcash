aba29927fc159d091f8b45ad26de5ebf30773f47
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-09 10:52:10 +0100

    fix: corrected typo

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -1,3 +1,3 @@
 export const appTitle = "Pay with Zcash";
 export const appDescription =
-  'Pay with Zcash" is a platform that lets users discover stores and businesses that accept Zcash as a payment method, making it simple to use cryptocurrency for everyday transactions and support businesses embracing digital currencies.';
+  "Pay with Zcash is a platform that lets users discover stores and businesses that accept Zcash as a payment method, making it simple to use cryptocurrency for everyday transactions and support businesses embracing digital currencies.";
