a66e62deb03d5f5e141c234e6c3ecbb287aa50ad
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-15 20:52:29 +0100

    fix: add new comp

--- src/app/ui/AdminDashboard.tsx
+++ src/app/ui/AdminDashboard.tsx
@@ -2,11 +2,13 @@
 import { useEffect, useState } from "react";
 import { Category } from "../lib/models/Category";
 import { Merchant, MerchantProps } from "../lib/models/Merchant";
+import { POST_STATUS_ENUM } from "../lib/typings";
 import CategoriesTable from "./dashboard/CategoriesTable";
 import MerchantsTable from "./dashboard/MerchantsTable";
 
 type AdminDashboardProps = {
   merchants: MerchantProps;
+  merchantsNotApproved: number;
   categories: Category[];
 };
 
@@ -30,6 +32,33 @@ const AdminDashboard = (props: AdminDashboardProps) => {
         >
           ☰
         </button>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-6 mb-8">
+          {/* <!-- Pending --> */}
+          <div className="bg-yellow-100 border-l-4 border-yellow-500 p-4 rounded shadow transition-all duration-300 hover:bg-yellow-200 hover:border-yellow-600">
+            <h2 className="text-yellow-800 font-semibold text-lg">
+              Pending Approvals:{props.merchantsNotApproved}
+            </h2>
+          </div>
+          <div className="bg-green-100 border-l-4 border-green-500 p-4 rounded shadow transition-all duration-300 hover:bg-green-200 hover:border-green-600">
+            <h2 className="text-green-800 font-semibold text-lg">
+              Approved:{" "}
+              {props.merchants.data.reduce((acc, cur) => {
+                if (cur.post_status === POST_STATUS_ENUM.PUBLISH) {
+                  acc++;
+                }
+
+                return acc;
+              }, 0)}
+            </h2>
+          </div>
+
+          <div className="bg-gray-100 border-l-4 border-gray-500 p-4 rounded shadow transition-all duration-300 hover:bg-gray-200 hover:border-gray-600">
+            <h2 className="text-gray-800 font-semibold text-lg">
+              Total Merchants: {merchants.length}
+            </h2>
+          </div>
+        </div>
 
         <CategoriesTable categories={categories} />
         <MerchantsTable merchants={merchants} />
