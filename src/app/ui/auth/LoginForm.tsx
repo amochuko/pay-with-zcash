1e472fadbd12d12ba850c6d09fbf6cb4cec78114
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-15 09:29:01 +0100

    fix: adjust css for dark mode

--- src/app/ui/auth/LoginForm.tsx
+++ src/app/ui/auth/LoginForm.tsx
@@ -9,48 +9,44 @@ export default function LoginForm() {
   return (
     <form
       action={loginAction}
-      className="max-w-xl mx-auto p-6 bg-slate-700  shadow-lg rounded-lg relative top-28"
+      className="max-w-xl mx-auto p-12 bg-slate-200 dark:bg-slate-700 shadow-lg rounded-lg relative top-50"
     >
-      <h1 className="text-2xl">Login</h1>
+      <h1 className="text-2xl font-bold mb-12">Login</h1>
 
-      <div className="mb-4">
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          disabled={pending}
-          className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
-        />
-        {state?.errors?.email && (
-          <p className="text-red-400 mt-2">{state.errors.email}</p>
-        )}
-      </div>
+      <div className="flex flex-col gap-8">
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-bold sr-only">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email Address"
+            disabled={pending}
+            className="mt-1 block w-full px-4 py-4 text-lg border border-gray-600 dark:border-gray-300  rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
+          />
+          {state?.errors?.email && (
+            <p className="text-red-400 mt-2">{state.errors.email}</p>
+          )}
+        </div>
 
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          disabled={pending}
-          className="mt-1 block w-full px-4 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
-        />
-        {state?.errors?.password && (
-          <p className="text-red-400 mt-2">{state.errors.password}</p>
-        )}
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-sm font-bold sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            disabled={pending}
+            className="mt-1 block w-full px-4 py-4 text-lg border border-gray-600 dark:border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 disabled:bg-gray-100 disabled:text-gray-400"
+          />
+          {state?.errors?.password && (
+            <p className="text-red-400 mt-2">{state.errors.password}</p>
+          )}
+        </div>
       </div>
 
       <button
