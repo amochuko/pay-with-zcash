c6300c39305ea8a61f23fb2cd2c8690ede20f221
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-15 13:36:32 +0100

    update: make email optional

--- src/app/lib/typings.ts
+++ src/app/lib/typings.ts
@@ -56,10 +56,7 @@ export type LoginStateForm =
   | undefined;
 
 export const signInSchema = z.object({
-  email: z
-    .string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
+  email: z.string().email("Invalid email").optional(),
   password: z
     .string({ required_error: "Password is required" })
     .min(1, "Password is required")
