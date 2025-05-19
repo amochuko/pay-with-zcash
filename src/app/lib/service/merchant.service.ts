757aba0a219f0ead0e6125d3ed63be07e01d6b8f
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-19 17:11:50 +0100

    fix: update order by

--- src/app/lib/service/merchant.service.ts
+++ src/app/lib/service/merchant.service.ts
@@ -39,21 +39,18 @@ class MerchantService {
     }
   }
 
-  async getMerchants({
-    approved = true
-  }: {
-    approved: boolean;
-  }): Promise<Merchant[]> {
+  async getMerchants(): Promise<Merchant[]> {
     try {
-      const res =
-        await sql(`SELECT m.*, c.category_name, i.img_name, i.img_bin_data 
+      const res = await sql(
+        `SELECT m.*, c.category_name, i.img_name, i.img_bin_data 
                         FROM public.merchants AS m
                         INNER JOIN categories AS c
                         ON m.category_id = c.category_id
                         LEFT JOIN logo_images AS i
                         ON m.logo_img_id = i.img_id
-                        ${approved && `WHERE m.post_status = 'publish'`}
-                        ORDER BY c.category_name ASC;`);
+                    
+                        ORDER BY c.category_name ASC, m.created_at DESC;`
+      );
 
       if (res.rowCount && res.rowCount > 1) {
         return res.rows;
