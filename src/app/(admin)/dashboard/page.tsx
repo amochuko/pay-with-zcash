3c6f326c00e763b786dca88d72fce9e8ec1b6cfa
amochuko <7204868+amochuko@users.noreply.github.com>
2025-08-10 07:21:04 +0100

    update: refactor for clean up

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,18 +1,10 @@
 import { getAllCategory } from "@/app/actions/category.action";
-import { getMerchants, getMerchantsBy } from "@/app/actions/merchant.action";
-import { POST_STATUS_ENUM } from "@/app/lib/typings";
+import { getMerchants } from "@/app/actions/merchant.action";
 import AdminDashboard from "@/app/ui/AdminDashboard";
 
 export default async function DashboardPage() {
   const merchants = await getMerchants();
-  const merchantsNotApproved = await getMerchantsBy(POST_STATUS_ENUM.DRAFT);
   const categories = await getAllCategory();
 
-  return (
-    <AdminDashboard
-      merchants={merchants}
-      categories={categories}
-      merchantsNotApproved={merchantsNotApproved.length}
-    />
-  );
+  return <AdminDashboard merchants={merchants} categories={categories} />;
 }
